4a48ee935212cffc5b03982d44673411fbde75b1
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2023-04-04 16:28:34 +0600

    basic route setup added

---
+++ src/Layout/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Pages/Shared/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
