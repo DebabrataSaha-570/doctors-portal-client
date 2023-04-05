f694e6374cce693d901822d18dafd0b9b8e5fdec
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2023-04-05 20:03:52 +0600

    testimonial section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from "./Banner";
 import InfoCards from "./InfoCards";
 import Services from "./Services";
 import MakeAppointment from "./MakeAppointment";
+import Testimonial from "./Testimonial/Testimonial";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <InfoCards></InfoCards>
       <Services></Services>
       <MakeAppointment></MakeAppointment>
+      <Testimonial></Testimonial>
     </div>
   );
 };
